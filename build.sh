25d0e13d7f68c1e3c2c58340c9ab3f96f0e5b931
Ja1ter <anapat5937@gmail.com>
2023-04-03 11:43:46 +0700

    LAPTOP-3/4/2023-11:43

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 mkdir bin
+mkdir out
 make all
 cp prog bin/prog
 rm prog
 make clean
 ./bin/prog
 Rscript plot.R
+cp output.txt out/output.txt
+rm output.txt
 dot -Tpng diagram.dot > image.png
+pdflatex createPdf.tex
+cp Rplots.pdf out/Rplots.pdf
+cp image.png out/image.png
+cp createPdf.aux out/createPdf.aux
+cp createPdf.log out/createPdf.log
+rm Rplots.pdf
+rm image.png
+rm createPdf.aux
+rm createPdf.log
